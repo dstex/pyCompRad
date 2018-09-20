6dc6756845162f5f249416f9327997519edca8af
Dan Stechman <stechman@ou.edu>
2018-09-20 10:53:00 -0500

    Update

--- RemoveMacCrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remove the .DS_Store and ._* files that transfer over from Mac
-# Place this file in the parent data directory, and it will recursively remove
-# those pesky files from the parent and all subdirectories
-
-find . \( -name '.DS_Store' -or -name '._*' \) -delete
